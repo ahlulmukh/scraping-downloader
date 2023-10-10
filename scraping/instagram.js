14a3a8fc27d47ef36e6db67be05861be57169028
Ahlul <ahlul963@gmail.com>
2023-10-10 15:14:30 +0700

    add scraping instagram

--- scraping/instagram.js
+++ scraping/instagram.js
@@ -1,11 +1,14 @@
 const { default: axios } = require("axios");
 const cheerio = require("cheerio");
 
-function facebook(url) {
+function instagram(url) {
   return new Promise((resolve, reject) => {
     let config = {
       url: url,
+      lang_code: "en",
+      token: "",
     };
+
     let headerss = {
       "sec-ch-ua":
         '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
@@ -14,45 +17,32 @@ function facebook(url) {
       Cookie:
         'PHPSESSID=6jo2ggb63g5mjvgj45f612ogt7; _ga=GA1.2.405896420.1625200423; _gid=GA1.2.2135261581.1625200423; _PN_SBSCRBR_FALLBACK_DENIED=1625200785624; MarketGidStorage={"0":{},"C702514":{"page":5,"time":1625200846733}}',
     };
-    axios("https://www.getfvid.com/downloader", {
+    axios("https://fastdl.app/c/", {
       method: "POST",
       data: new URLSearchParams(Object.entries(config)),
       headers: headerss,
     })
       .then(({ data }) => {
         const $ = cheerio.load(data);
-        const title = $(".card-title a").text().trim();
-        const links = {
-          hd: "",
-          sd: "",
-          audio: "",
-        };
+        const downloadLinks = [];
 
-        $(".btns-download a").each((index, element) => {
-          const link = $(element).attr("href");
-          const text = $(element).text(); // Ambil teks dari link untuk menentukan jenis link
-          if (text.includes("HD Quality")) {
-            links.hd = link;
-          } else if (text.includes("Normal Quality")) {
-            links.sd = link;
-          } else if (text.includes("Audio Only")) {
-            links.audio = link;
-          }
+        // Iterate over all anchor elements with an href attribute
+        $("a[href]").each((index, element) => {
+          const hrefValue = $(element).attr("href");
+          downloadLinks.push(hrefValue);
         });
 
-        resolve({
-          status: 200,
-          title: title,
-          hd: links.hd,
-          sd: links.sd,
-          audio: links.audio,
-        });
+        resolve(downloadLinks);
       })
-      .catch(reject);
+      .catch(({ error }) => {
+        reject(error);
+      });
   });
 }
 
-facebook("https://fb.watch/7B5KBCgdO3")
+instagram(
+  "https://www.instagram.com/reel/CwOMK_ohU85/?utm_source=ig_web_copy_link"
+)
   .then((result) => {
     console.log(result);
   })
